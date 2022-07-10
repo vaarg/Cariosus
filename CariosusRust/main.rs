c4f978b32a8b27fb47b1e50a361d58c1d9f6fd0b
vaarg <104344158+vaarg@users.noreply.github.com>
2022-07-11 02:23:23 +1000

    Update main.rs

--- CariosusRust/main.rs
+++ CariosusRust/main.rs
@@ -1,126 +1,128 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
-use std::process;
 
-//
-// UNDER CONSTRUCTION
-//
+#[allow(dead_code)]
 
-fn login() {
+pub struct User {
+    id: u64,
+    username: String,
+    password: String,
+    administrator: bool,
+}
 
+pub struct Users {
+    store: HashMap<String, User>,
+}
+
+impl Users {
+    pub fn new() -> Users {
+        Users { store: HashMap::new() }
+    }
+
+    pub fn join(&mut self, user: User) {
+        self.store.insert(user.username.clone(), user);
+    }
+
+    pub fn check_login(self, username: String, password: String) -> bool {
+        if let Some(u) = self.store.get(username.as_str()) {
+            return u.password == password;
+        } else {
+            false
+        }
+    }
+}
+
+fn login(users: Users) {
+    
     print!("Enter username: ");
     io::stdout().flush().unwrap();
     let mut username = String::new();
-
     io::stdin()
         .read_line(&mut username)
-        .expect("Error getting Username");
-
+        .expect("Option error!");
+    let username = username.trim();
+    
     print!("Enter password: ");
     io::stdout().flush().unwrap();
     let mut password = String::new();
-
     io::stdin()
         .read_line(&mut password)
-        .expect("Error getting Password");
+        .expect("Option error!");
+    let password = password.trim();
 
-    println!("Your credentials are {}:{}", username.trim(), password.trim());
-
+    if users.check_login(username.to_string(), password.to_string()) {
+        println!("Welcome {username}!");
+    } else {
+        println!("Invalid login!");
+    }
 }
 
-fn register() {
-    
-    print!("Enter name: ");
-    io::stdout().flush().unwrap();
-    let mut name = String::new();
-
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Error getting Name");
-
-    print!("Enter age: ");
-    io::stdout().flush().unwrap();
-    let mut age = String::new();
-    io::stdin()
-        .read_line(&mut age)
-        .expect("Error getting Age");
-    let age = age.trim();
-    let int_age = match age.parse::<i32>() {
-        Ok(int_age) => int_age,
-        Err(_e) => -1,
-    };
-    if int_age < 0 {
-        println!("\nInvalid age entered!\n");
-        process::exit(1);
-    }
+fn register(mut users: Users, id: u64) {
     
     print!("Enter username: ");
     io::stdout().flush().unwrap();
-    let mut username = String::new();
+    let mut username = String:: new();
     io::stdin()
         .read_line(&mut username)
-        .expect("Error getting Username");
+        .expect("Username error.");
+    let username = username.trim();
+    
+    let password = loop {
+        print!("Enter password: ");
+        io::stdout().flush().unwrap();
+        let mut password = String::new();
+        io::stdin()
+            .read_line(&mut password)
+            .expect("Password error.");
+        
+        print!("Confirm password: ");
+        io::stdout().flush().unwrap();
+        let mut password2 = String::new();
+        io::stdin()
+            .read_line(&mut password2)
+            .expect("Password error.");
 
-    print!("Enter password: ");
-    io::stdout().flush().unwrap();
-    let mut password1 = String::new();
-    io::stdin()
-        .read_line(&mut password1)
-        .expect("Error getting Password");
+        if password.eq(&password2) {
+            break password;
+        } else {
+            println!("Passwords don't match!");
+            continue;
+        }      
+    };
+    let password = password.trim();
+    let id = id + 1;
 
-    print!("Confirm password: ");
-    io::stdout().flush().unwrap();
-    let mut password2 = String::new();
-    io::stdin()
-        .read_line(&mut password2)
-        .expect("Error getting Password");
-
-    if password1 != password2 {
-        println!("Passwords do not match!");
-        process::exit(1);
-    }
-    else {
-        println!("Creating account {}!", username.trim());
-        let _new_user = vec![
-            Users::Name(name),
-            Users::Age(int_age),
-            Users::Username(username),
-            Users::Password(password1),
-        ];
-    }
-
-}
-
-enum Users {
-
-    Name(String), //Name
-    Age(i32), //Age
-    Username(String), //Username
-    Password(String), //Password
+    users.join(User {id: id, username: username.to_string(), password: password.to_string(), administrator: false});
+    // println!("Found {:?} entries", users.store.len());
+    // assert_eq!(2, users.store.len())
+    login(users);
 
 }
 
 fn main() {
+    
+    let id = 1;
+    
+    let mut users = Users::new();
+    users.join(User {id: id, username: "Admin".to_string(), password: "Admin".to_string(), administrator: true});
 
-    println!("\nWelcome to Cariosus Corp!\n\nWould you like to:\n[1] Login\n[2] Register");
-    print!("\nEnter option [1] or [2]: ");
+
+    print!("\nWelcome to Cariosus Corp!\n\nWould you like to:\n[1] Login\n[2] Register\n\nEnter option [1] or [2]: ");
     io::stdout().flush().unwrap();
     let mut option = String::new();
-
     io::stdin()
         .read_line(&mut option)
         .expect("Option error!");
     let option = option.trim();
 
     if option == "1" {
-        login();
-    } 
-    else if option == "2" {
-        register();
-    } 
-    else {
+        login(users);
+    } else if option == "2" {
+        register(users, id);
+        // login(users);
+    } else {
         println!("Invalid option!");
     }
 
 }
-
